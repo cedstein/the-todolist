809f1ec4c0880d4a16cbdc3dd089a03a68dd2d3a
Cecilia Edstein <cecilia.edstein@gmail.com>
2025-10-27 14:31:20 +0100

    created my to do list

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import "./style.css";
-import "./to-do";
+import { ToDo } from "./to-do";
 // Import our custom CSS
 // import "../scss/styles.scss";
+
+/* const myToDo = new ToDo("städa"); */
+
+const myToDoList = [
+  new ToDo("Koda mer"),
+  new ToDo("lära mig mer om Javascript"),
+  new ToDo("Lära mig mer om ramverk"),
+];
--- src/to-do.js
+++ src/to-do.js
@@ -1,4 +1,4 @@
-export class toDo {
+export class ToDo {
   text;
 
   constructor(text) {
