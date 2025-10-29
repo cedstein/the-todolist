db45bcde857175e4c61f95765760391bbd9f0635
Cecilia Edstein <cecilia.edstein@gmail.com>
2025-10-29 14:09:33 +0100

    handle Submit click and remove

--- src/main.js
+++ src/main.js
@@ -4,12 +4,23 @@ import { createHtml } from "./utils";
 
 // Import our custom CSS
 // import "../scss/styles.scss";
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const newToDo = new ToDo("text");
+  myToDoList.push(newToDo);
+  createHtml(myToDoList);
+};
 
 const myToDoList = [
   new ToDo("Koda mer"),
   new ToDo("Lära mig mer om Javascript"),
   new ToDo("Lära mig mer om ramverk"),
 ];
+
+const form = document.getElementById("toDoForm");
+if (form !== null) {
+  form.addEventListener("submit", handleSubmit);
+}
 
 /* let toDoes []; */
 
--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,3 @@
-/* export const createHtml = (toDoes) => {
-  const ulElement = document.getElementById("toDoes");
-  const li = document.createElement("li");
-
-  li.innerHTML = toDoes.li;
-
-  ulElement.appendChild.apply("li");
-
-  for (let i = 0; < myToDoList.length; i++) {
-    const li = document.createElement("li");
-    li.innerHTML = myToDoList[i];
-    ulElement.appendChild(li);
-    
- }
-};
-
-createHtml("toDoes"); */
-
 export const createHtml = (myToDoList) => {
   const ulElement = document.getElementById("toDoes");
   ulElement.innerHTML = "";
